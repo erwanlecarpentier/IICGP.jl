4a71913a5f3b40e8c69e4c087c39ea71a6de8749
erwanlecarpentier <erwan.lc2@gmail.com>
2021-03-23 16:49:17 +0100

    Sandbox

---
+++ sandbox.jl
@@ -0,0 +1,14 @@
+t = Template(;
+    user="erwanlecarpentier",
+    license="MIT",
+    authors="Erwan Lecarpentier",
+    julia=v"1.5",
+    plugins=[
+        License(; name="MPL"),
+        Git(; manifest=true, ssh=true),
+        GitHubActions(; x86=true),
+        Codecov(),
+        Documenter{GitHubActions}(),
+        Develop(),
+    ],
+)
