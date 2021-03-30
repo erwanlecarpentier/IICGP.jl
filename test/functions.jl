a8a83d24d89b4cf38dcf64e38946acd4f9f33d73
erwanlecarpentier <erwan.lc2@gmail.com>
2021-03-30 19:37:59 +0200

    More testing on split i/o.

--- test/functions.jl
+++ test/functions.jl
@@ -71,9 +71,13 @@ end
 image_name = "centipede_frame_0"
 filename = string(@__DIR__, "/", image_name, ".png")
 atari_img = OpenCV.imread(filename)
+grsca_img = OpenCV.cvtColor(atari_img, OpenCV.COLOR_BGR2GRAY)
 noisy_img = generate_noisy_img()
 white_img = generate_white_img()
 black_img = generate_black_img()
+
+OpenCV.imshow("Image", grsca_img)
+OpenCV.waitKey(Int32(0))
 
 img_set = [atari_img, noisy_img, white_img, black_img]
 img_pairs = Combinatorics.combinations(img_set, 2)
@@ -86,7 +90,8 @@ end
 
 test_img = Array{UInt8,2}[atari_img[1,:,:]]
 
-# split(m::InputArray, mv::Array{InputArray, 1})
+
+
 out = Array{OpenCV.InputArray}[]
 out = Array{OpenCV.Mat{UInt8}, 1}[]
 out = Array{Array{UInt8,1}, 1}[]
@@ -99,9 +104,37 @@ out = Array{UInt8,1}[]
 
 out = Array{Array{UInt8,1}}(undef, 3)
 out = Array{OpenCV.InputArray}(undef, 3)
+out = Array{OpenCV.Mat{UInt8}}(undef, 3)
 
+out = Array{OpenCV.InputArray, 1}()
+out = Array{OpenCV.Mat{UInt8}, 1}()
+
+# split(m::InputArray, mv::Array{InputArray, 1})
 OpenCV.split(noisy_img, out)
 OpenCV.split(atari_img, out)
+
+
+using OpenCV
+
+noisy_img = rand(collect(UInt8, 0:255), (3, 320, 210))
+
+out = OpenCV.CxxMat[]
+out = AbstractArray{UInt8,3}[]
+out = OpenCV.Mat{UInt8}[]
+out = Array{UInt8}[]
+out = Array{OpenCV.Mat{UInt8}, 1}()
+out = OpenCV.InputArray[]
+out = Array{OpenCV.InputArray, 1}()
+
+OpenCV.split(noisy_img, out)
+
+OpenCV.split(atari_img, out)  # Split RGB channels
+OpenCV.split(atari_img)
+
+# const dtypes = Union{UInt8, Int8, UInt16, Int16, Int32, Float32, Float64}
+# const InputArray = Union{AbstractArray{T, 3} where {T <: dtypes}, CxxMat}
+
+
 
 typeof(noisy_img) <: OpenCV.InputArray
 typeof(out) <: Array{OpenCV.InputArray, 1}
