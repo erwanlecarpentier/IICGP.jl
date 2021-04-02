52f0df540a40c25013e3dbf0922c51e12b4954a9
erwanlecarpentier <erwan.lc2@gmail.com>
2021-04-02 19:23:43 +0200

    clean-up

--- sandbox.jl
+++ sandbox.jl
@@ -1,9 +1,14 @@
-global buffer
-buffer = 0
+# global buffer
+# buffer = 0
 
 function foo()
-    global buffer
-    buffer += 1
+    if @isdefined buffer
+        global buffer
+        buffer += 1
+    else
+        global buffer
+        buffer = 1
+    end
 end
 
 foo()
