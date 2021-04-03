d8b454e0e0794415adc941b8c14b1c19f3d4d249
erwanlecarpentier <erwan.lc2@gmail.com>
2021-04-03 12:28:51 +0200

    Benchmark on functions with internal state

--- sandbox.jl
+++ sandbox.jl
@@ -1,14 +1,49 @@
+using BenchmarkTools
+
+
 # global buffer
 # buffer = 0
 
-function foo()
-    if @isdefined buffer
-        global buffer
-        buffer += 1
+function foobar()
+    if @isdefined foobar_buffer
+        global foobar_buffer
+        foobar_buffer += 1
     else
         global buffer
-        buffer = 1
+        foobar_buffer = 1
     end
 end
 
-foo()
+foobar()
+
+@btime foobar()
+
+
+
+f(state=0) = ()->state+=1
+
+
+let state = Ref{Union{Int, Nothing}}(nothing)
+    global bar
+    function bar()
+        if state[] !== nothing
+            state[] += 1
+        else
+            state[] = 1
+        end
+        state[]
+    end
+ end
+
+Base.@kwdef mutable struct F
+    state = nothing
+end
+
+function (o::F)()
+    if o.state == nothing
+        o.state = 1
+    else
+        o.state += 1
+    end
+end
+fi = F()
