2d2e82bd7e43a5e685a5a16a40e8fb49bf3230f4
erwanlecarpentier <erwan.lc2@gmail.com>
2021-04-03 12:29:18 +0200

    motion_capture function with internal state as global mutable

--- examples/motion_capture.jl
+++ examples/motion_capture.jl
@@ -1,10 +1,39 @@
 using OpenCV
 using IICGP
 
-
+"""
 function motion_capture(m::T, buffer::T) where {T <: OpenCV.InputArray}
     OpenCV.subtract(m, buffer)
 end
+"""
+
+# method with global mutable variable
+function motion_capture(m::T) where {T <: OpenCV.InputArray}
+    if @isdefined global_mutable_mc_buffer
+        global global_mutable_mc_buffer
+        return OpenCV.subtract(m, global_mutable_mc_buffer)
+    else
+        global global_mutable_mc_buffer
+        global_mutable_mc_buffer = m
+        return nothing
+    end
+end
+
+
+# method with `let` to hide the internal state
+let buffer = Ref{Union{<:OpenCV.InputArray, Nothing}}(nothing)
+    global motion_capture
+    function motion_capture(m::T) where {T <: OpenCV.InputArray}
+        if buffer[] !== nothing
+            return OpenCV.subtract(m, buffer)
+        else
+            buffer = m
+            return nothing
+        end
+    end
+ end
+
+
 
 function load_img(rom_name::String, frame_number::Int64)
     filename = string(@__DIR__, "/images/", rom_name, "_frame_$frame_number.png")
@@ -15,8 +44,8 @@ rom_name = "freeway"
 
 # First input
 img = load_img(rom_name, 30)
+IICGP.imshow(img)
 out = motion_capture(img)
-IICGP.imshow(out)
 
 # Second input
 img = load_img(rom_name, 31)
