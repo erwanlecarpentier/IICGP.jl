65039d1019c70ed51f70598ec446074654db40ca
erwanlecarpentier <erwan.lc2@gmail.com>
2021-04-06 12:08:42 +0200

    Pattern recognition map

--- examples/pattern_recognition.jl
+++ examples/pattern_recognition.jl
@@ -1,11 +1,36 @@
 using OpenCV
 using IICGP
 
+"""
+   pattern_recognition(m::T; ksize::Int64=100, pos::Tuple{Int64,Int64}=(1, 1)) where {T <: OpenCV.InputArray}
+
+Pattern recognition method.
+Method:
+- TM_CCOEFF
+- TM_CCOEFF_NORMED
+- TM_CCORR
+- TM_CCORR_NORMED
+- TM_SQDIFF
+- TM_SQDIFF_NORMED
+"""
 function pattern_recognition(m::T; ksize::Int64=100, pos::Tuple{Int64,Int64}=(1, 1)) where {T <: OpenCV.InputArray}
    x = min(pos[1], size(img)[2] - ksize + 1)
    y = min(pos[2], size(img)[3] - ksize + 1)
-   m = m[:, x:x+ksize-1, y:y+ksize-1]
-   IICGP.imshow(m)
+   template = m[:, x:x+ksize-1, y:y+ksize-1]
+
+
+   IICGP.imshow(template)
+
+
+   println()
+   # println(x:x+ksize-1, " ", y:y+ksize-1)
+
+   match = OpenCV.matchTemplate(m, template, OpenCV.TM_CCOEFF_NORMED)
+
+   # println(size(match))
+   IICGP.imshow(match)
+
+   return match
 end
 
 function load_img(rom_name::String, frame_number::Int64)
@@ -21,6 +46,6 @@ img = IICGP.split_rgb(img)[1]
 IICGP.imshow(img)
 
 out = pattern_recognition(img)
-out = pattern_recognition(img, ksize=100, pos=(300, 1))
+out = pattern_recognition(img, ksize=100, pos=(100, 50))
 
 IICGP.imshow(out)
