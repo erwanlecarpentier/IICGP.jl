9c10400b6fd603fb2397e9c53938db3b1e36fb5d
erwanlecarpentier <erwan.lc2@gmail.com>
2021-04-15 19:15:43 +0200

    Meta Module handling example

--- sandbox.jl
+++ sandbox.jl
@@ -1,7 +1,35 @@
-bar(x::Int64, y::Int64) = x + y
 
-function foobar(n::Int64=3)
-    for i in 1:n
-        println(bar(i, i))
+function foo(x::String="Ehoy"; kwargs...)
+    println()
+    println(x)
+    println(typeof(kwargs))
+
+    for (k, v) in kwargs
+        println(k, " ", v)
     end
+
+    return kwargs
 end
+
+out = foo(y=2, z="Hola")
+println(out)
+
+module AFoo
+function foo()
+    println("A foo")
+end
+end
+
+module B
+function foo()
+    println("B foo")
+end
+end
+
+function bar(m::Module)
+    m.foo()
+    println("Used module: ", string(:($m)))
+end
+
+bar(AFoo)
+bar(B)
