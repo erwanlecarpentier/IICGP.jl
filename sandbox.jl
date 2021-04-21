3cdba736830a39d57ab043052352936488ed6097
erwanlecarpentier <erwan.lc2@gmail.com>
2021-04-21 15:52:59 +0200

    Sandbox

--- sandbox.jl
+++ sandbox.jl
@@ -1,35 +1,43 @@
 
-function foo(x::String="Ehoy"; kwargs...)
-    println()
-    println(x)
-    println(typeof(kwargs))
-
-    for (k, v) in kwargs
-        println(k, " ", v)
-    end
-
-    return kwargs
-end
-
-out = foo(y=2, z="Hola")
-println(out)
-
-module AFoo
+module A
 function foo()
-    println("A foo")
+    println("A.foo()")
 end
 end
 
 module B
 function foo()
-    println("B foo")
+    println("B.foo()")
 end
 end
 
 function bar(m::Module)
     m.foo()
-    println("Used module: ", string(:($m)))
 end
 
-bar(AFoo)
+bar(A)
 bar(B)
+
+
+
+
+
+
+using PkgTemplates
+
+t = Template(;
+    user="erwanlecarpentier",
+    authors="Erwan Lecarpentier",
+    julia=v"1.5.4",
+    plugins=[
+        Codecov(),
+        Coveralls(),
+        License(; name="MIT"),
+        Git(; manifest=true, ssh=false),
+    ],
+)
+
+# https://www.youtube.com/watch?v=QVmU29rCjaA&t=1356s
+# 7:18
+
+generate("JuliaSandbox", t)
