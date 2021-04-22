89aeda9fc493215dbefc892b41a14ab7b1cd286b
erwanlecarpentier <erwan.lc2@gmail.com>
2021-04-22 17:32:52 +0200

    Include reducer

--- src/IICGP.jl
+++ src/IICGP.jl
@@ -7,6 +7,7 @@ import JSON
 # include("config.jl")
 # include("evolution.jl")
 include("functions.jl")
+include("reducer.jl")
 # include("individual.jl")
 # include("process.jl")
 include("utils.jl")
