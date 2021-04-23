5e779e2b1669ddd9f9aa4d013149e2beaa0dd7ac
erwanlecarpentier <erwan.lc2@gmail.com>
2021-04-23 14:37:05 +0200

    Ratio-parametrized imshow in utils

--- src/utils.jl
+++ src/utils.jl
@@ -15,9 +15,25 @@ end
 """
     imshow(m::T) where {T <: OpenCV.InputArray}
 
-Show input image using functions `imshow` and `waitKey` from OpenCV Julia binding.
+Show input image using functions `imshow` and `waitKey` from OpenCV Julia
+binding.
 """
 function imshow(m::T) where {T <: OpenCV.InputArray}
     OpenCV.imshow("Image", m)
     OpenCV.waitKey(Int32(0))
+end
+
+"""
+    imshow(m::T; enlargement::Int64) where {T <: OpenCV.InputArray}
+
+Show enlarged input image using functions `imshow` and `waitKey` from OpenCV
+Julia binding.
+The enlargement factor is given by the `enlargement` input parameter.
+"""
+function imshow(m::T, enlargement::Float64) where {T <: OpenCV.InputArray}
+    n_cols = convert(Int32, size(m)[2] * enlargement)
+    n_rows = convert(Int32, size(m)[3] * enlargement)
+    new_size = OpenCV.Size(n_cols, n_rows)
+    enlarged = OpenCV.resize(m, new_size, m, 1.0, 1.0, OpenCV.INTER_NEAREST)
+    imshow(enlarged)
 end
