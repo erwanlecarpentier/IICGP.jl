8de708dd4a0a4fdddf290c5c8b299191a65be539
erwanlecarpentier <erwan.lc2@gmail.com>
2021-04-23 15:21:56 +0200

    Implemented nearest, linear, area, cubic and Lanczos reductions

--- src/reducer.jl
+++ src/reducer.jl
@@ -4,6 +4,12 @@ module ReducingFunctions
 
 using OpenCV
 
+function nearest_reduction(m::T; s::Int64=5) where {T <: OpenCV.InputArray}
+    # dsize = OpenCV.Size(convert(Int32, s), convert(Int32, s))
+    return OpenCV.resize(m, OpenCV.Size(convert(Int32, s), convert(Int32, s)),
+                         m, 1.0, 1.0, OpenCV.INTER_NEAREST)
+end
+
 function linear_reduction(m::T; s::Int64=5) where {T <: OpenCV.InputArray}
     # dsize = OpenCV.Size(convert(Int32, s), convert(Int32, s))
     return OpenCV.resize(m, OpenCV.Size(convert(Int32, s), convert(Int32, s)),
@@ -14,6 +20,18 @@ function area_reduction(m::T; s::Int64=5) where {T <: OpenCV.InputArray}
     # dsize = OpenCV.Size(convert(Int32, s), convert(Int32, s))
     return OpenCV.resize(m, OpenCV.Size(convert(Int32, s), convert(Int32, s)),
                          m, 1.0, 1.0, OpenCV.INTER_AREA)
+end
+
+function cubic_reduction(m::T; s::Int64=5) where {T <: OpenCV.InputArray}
+    # dsize = OpenCV.Size(convert(Int32, s), convert(Int32, s))
+    return OpenCV.resize(m, OpenCV.Size(convert(Int32, s), convert(Int32, s)),
+                         m, 1.0, 1.0, OpenCV.INTER_CUBIC)
+end
+
+function lanczos_reduction(m::T; s::Int64=5) where {T <: OpenCV.InputArray}
+    # dsize = OpenCV.Size(convert(Int32, s), convert(Int32, s))
+    return OpenCV.resize(m, OpenCV.Size(convert(Int32, s), convert(Int32, s)),
+                         m, 1.0, 1.0, OpenCV.INTER_LANCZOS4)
 end
 
 end
--- test/reducer.jl
+++ test/reducer.jl
@@ -24,8 +24,11 @@ end
         images = [r, g, b]
 
         functions = [
+                IICGP.ReducingFunctions.nearest_reduction,
+                IICGP.ReducingFunctions.linear_reduction,
                 IICGP.ReducingFunctions.area_reduction,
-                IICGP.ReducingFunctions.linear_reduction
+                IICGP.ReducingFunctions.cubic_reduction,
+                IICGP.ReducingFunctions.lanczos_reduction
         ]
 
         # Test each function
@@ -41,15 +44,21 @@ r, g, b = IICGP.split_rgb(m)
 images = [r, g, b]
 
 
-m_reduced = IICGP.ReducingFunctions.area_reduction(m)
+m_reduced = IICGP.ReducingFunctions.nearest_reduction(m)
 m_reduced = IICGP.ReducingFunctions.linear_reduction(m)
+m_reduced = IICGP.ReducingFunctions.area_reduction(m)
+m_reduced = IICGP.ReducingFunctions.cubic_reduction(m)
+m_reduced = IICGP.ReducingFunctions.lanczos_reduction(m)
 
 imshow(m_reduced, 100)
 
 
 
 functions = [
+        IICGP.ReducingFunctions.nearest_reduction,
+        IICGP.ReducingFunctions.linear_reduction,
         IICGP.ReducingFunctions.area_reduction,
-        IICGP.ReducingFunctions.linear_reduction
+        IICGP.ReducingFunctions.cubic_reduction,
+        IICGP.ReducingFunctions.lanczos_reduction
 ]
 test_functions(functions, images)
