eba09442dd95c7b256c1f4d554679b9eb5074de1
erwanlecarpentier <erwan.lc2@gmail.com>
2021-04-23 15:22:18 +0200

    Clean-up

--- test/reducer.jl
+++ test/reducer.jl
@@ -35,14 +35,13 @@ end
         test_functions(functions, images)
 end
 
-
+#=
 # Load image and split
 image_name = "centipede_frame_0"
 filename = string(@__DIR__, "/", image_name, ".png")
 m = OpenCV.imread(filename)
 r, g, b = IICGP.split_rgb(m)
 images = [r, g, b]
-
 
 m_reduced = IICGP.ReducingFunctions.nearest_reduction(m)
 m_reduced = IICGP.ReducingFunctions.linear_reduction(m)
@@ -51,14 +50,4 @@ m_reduced = IICGP.ReducingFunctions.cubic_reduction(m)
 m_reduced = IICGP.ReducingFunctions.lanczos_reduction(m)
 
 imshow(m_reduced, 100)
-
-
-
-functions = [
-        IICGP.ReducingFunctions.nearest_reduction,
-        IICGP.ReducingFunctions.linear_reduction,
-        IICGP.ReducingFunctions.area_reduction,
-        IICGP.ReducingFunctions.cubic_reduction,
-        IICGP.ReducingFunctions.lanczos_reduction
-]
-test_functions(functions, images)
+=#
