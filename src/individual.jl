373dd2fc1695c05ad1691520d2b5936a0a67449d
erwanlecarpentier <erwan.lc2@gmail.com>
2021-04-29 17:37:44 +0200

    IPCGP structure

--- src/individual.jl
+++ src/individual.jl
@@ -2,10 +2,19 @@ export IPCGPInd
 
 using CartesianGeneticProgramming
 
+struct IPCGPInd <: Cambrian.Individual
+    cgp_ind::CartesianGeneticProgramming.CGPInd
+end
+
+"""
+    function IPCGPInd(cfg::NamedTuple)
+
+Configuration-based constructor for IPCGP individual.
+"""
 function IPCGPInd(cfg::NamedTuple)
     buffer = Array{Array{UInt8, 3}}(undef, cfg.rows * cfg.columns + cfg.n_in)
     fill!(buffer, zeros(UInt8, cfg.img_size))
-    CartesianGeneticProgramming.CGPInd(cfg; buffer=buffer)
+    IPCGPInd(CartesianGeneticProgramming.CGPInd(cfg; buffer=buffer))
 end
 
 #=
