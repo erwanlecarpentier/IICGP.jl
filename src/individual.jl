acdfc9681b0e2598a2b28928866c947fc1543453
erwanlecarpentier <erwan.lc2@gmail.com>
2021-05-03 12:07:49 +0200

    IPCGP constructor based on cfg-chromosome tuple + doc + clean-up

--- src/individual.jl
+++ src/individual.jl
@@ -2,19 +2,33 @@ export IPCGPInd
 
 using CartesianGeneticProgramming
 
-#=
-struct IPCGPInd <: Cambrian.Individual
-    cgp_ind::CartesianGeneticProgramming.CGPInd
+"""
+    function image_buffer(cfg::NamedTuple)
+
+Image buffer constructor for IPCGP individuals.
+"""
+function image_buffer(cfg::NamedTuple)
+    buffer = Array{Array{UInt8, 3}}(undef, cfg.rows * cfg.columns + cfg.n_in)
+    fill!(buffer, zeros(UInt8, cfg.img_size))
+    return buffer
 end
-=#
 
 """
     function IPCGPInd(cfg::NamedTuple)
 
-Configuration-based constructor for IPCGP individual.
+Constructor for IPCGP individual based on configuration.
 """
 function IPCGPInd(cfg::NamedTuple)
-    buffer = Array{Array{UInt8, 3}}(undef, cfg.rows * cfg.columns + cfg.n_in)
-    fill!(buffer, zeros(UInt8, cfg.img_size))
+    buffer = image_buffer(cfg)
     CartesianGeneticProgramming.CGPInd(cfg; buffer=buffer)
+end
+
+"""
+    function IPCGPInd(cfg::NamedTuple, chromosome::Array{Float64})
+
+Constructor for IPCGP individual based on configuration and chromosome.
+"""
+function IPCGPInd(cfg::NamedTuple, chromosome::Array{Float64})
+    buffer = image_buffer(cfg)
+    CartesianGeneticProgramming.CGPInd(cfg, chromosome; buffer=buffer)
 end
