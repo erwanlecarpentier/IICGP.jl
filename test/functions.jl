9c5f63dea1195468513e57aefdfe836050793622
erwanlecarpentier <erwan.lc2@gmail.com>
2021-05-18 15:37:45 +0200

    Test for motion capture

--- test/functions.jl
+++ test/functions.jl
@@ -16,24 +16,6 @@ function generate_noisy_img(sz::Tuple{Int64,Int64}=(210, 320))
     rand(collect(UInt8, 0:255), sz)
 end
 
-function test_inputs(f::Function, inps::AbstractArray)
-    out = copy(f(inps...))
-    @test size(out) == size(inps[1])
-    @test size(out) == size(inps[2])
-    @test typeof(out) == Array{UInt8,2}
-    @test all(out == f(inps...)) # functions are idempotent
-    @test all(out .>= 0)
-    @test all(out .<= 255)
-end
-
-function test_functions(functions::Array{Function}, pairs)
-    for f in functions
-        for p in pairs
-            test_inputs(f, p)
-        end
-    end
-end
-
 function generate_exp_img_set()
     set = Array{Array{UInt8,2},1}()
     # Min / Max / Random images
@@ -45,78 +27,69 @@ function generate_exp_img_set()
     set
 end
 
-function generate_atari_img_set()
+function generate_atari_img_set(frame_number::Int64)
     set = Array{Array{UInt8,2},1}()
     rom_list = setdiff(getROMList(), ["pacman", "surround"])
     for rom in rom_list
-        append!(set, IICGP.load_rgb(rom, 30))
+        append!(set, IICGP.load_rgb(rom, frame_number))
     end
     set
 end
 
 function generate_img_pair_set()
     exp_set = generate_exp_img_set()
-    atari_set = generate_atari_img_set()
-    exp_set = append!(exp_set, atari_set[1:6])
+    atari_set_30 = generate_atari_img_set(30)
+    atari_set_31 = generate_atari_img_set(31)
+    exp_set = append!(exp_set, atari_set_30[4:9])
     img_pairs = collect(Combinatorics.combinations(exp_set, 2))
-    n_img = length(atari_set)
-    for img in atari_set
-        append!(img_pairs, [[img, atari_set[rand(1:n_img)]]])
+    for i in eachindex(atari_set_30)
+        append!(img_pairs, [[atari_set_30[i], atari_set_31[i]]])
     end
     img_pairs
 end
 
+function test_inputs(f::Function, inps::AbstractArray; idempotent::Bool=true)
+    p = rand(1)
+    out = copy(f(inps..., p))
+    @test size(out) == size(inps[1])
+    @test size(out) == size(inps[2])
+    @test typeof(out) == Array{UInt8,2}
+    if idempotent
+        @test all(out == f(inps..., p))
+    end
+    @test all(out .>= 0)
+    @test all(out .<= 255)
+end
+
+function test_functions(functions::Array{Function},
+                        pairs::Array{Array{Array{UInt8,2},1},1};
+                        idempotent::Bool=true)
+    for f in functions
+        for p in pairs
+            test_inputs(f, p, idempotent=idempotent)
+        end
+    end
+end
+
 @testset "Julia Image functions" begin
+    # Fetch functions
+    idempotent_functions = [
+        IICGP.CGPFunctions.f_dilate,
+        IICGP.CGPFunctions.f_erode,
+        IICGP.CGPFunctions.f_remove_details,
+        IICGP.CGPFunctions.f_subtract
+    ]
+    non_idempotent_functions = [
+        IICGP.CGPFunctions.f_subtract,
+        IICGP.CGPFunctions.f_motion_capture
+    ]
+
     # Load / generate images
     pairs = generate_img_pair_set()
 
-    # Fetch functions
-    functions = [
-        IICGP.CGPFunctions.f_dilate,
-        IICGP.CGPFunctions.f_erode,
-        IICGP.CGPFunctions.f_remove_details
-    ]
+    # Test idempotent functions
+    test_functions(idempotent_functions, pairs, idempotent=true)
 
-    # Test all functions
-    test_functions(functions, pairs)
+    # Test non-idempotent functions
+    test_functions(non_idempotent_functions, pairs, idempotent=false)
 end
-
-#=
-@deprecated
-@testset "OpenCV functions" begin
-    # Load / generate images
-    image_name = "centipede_frame_0"
-    filename = string(@__DIR__, "/", image_name, ".png")
-    atari_img = OpenCV.imread(filename)
-    # grsca_img = OpenCV.cvtColor(atari_img, OpenCV.COLOR_BGR2GRAY)
-    sz = (1, 320, 210)
-    noisy_img = generate_noisy_img(sz)
-    white_img = generate_white_img(sz)
-    black_img = generate_black_img(sz)
-
-    img_set = vcat(split_rgb(atari_img), [noisy_img, white_img, black_img])
-    img_pairs = Combinatorics.combinations(img_set, 2)
-
-    # Fetch functions
-    functions = [
-        IICGP.CGPFunctions.f_absdiff_img,
-        IICGP.CGPFunctions.f_add_img,
-        IICGP.CGPFunctions.f_subtract_img,
-        IICGP.CGPFunctions.f_addweighted_img,
-        IICGP.CGPFunctions.f_bitwise_and_img,
-        IICGP.CGPFunctions.f_bitwise_not_img,
-        IICGP.CGPFunctions.f_bitwise_or_img,
-        IICGP.CGPFunctions.f_bitwise_xor_img,
-        IICGP.CGPFunctions.f_compare_eq_img,
-        IICGP.CGPFunctions.f_compare_ge_img,
-        IICGP.CGPFunctions.f_compare_le_img,
-        IICGP.CGPFunctions.f_max_img,
-        IICGP.CGPFunctions.f_min_img,
-        IICGP.CGPFunctions.f_dilate_img,
-        IICGP.CGPFunctions.f_erode_img
-    ]
-
-    # Test all functions
-    test_functions(functions, img_pairs)
-end
-=#
