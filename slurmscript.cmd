5957b9918e2b045bc6072bdd1cd720955a433956
3396 <lecarpen@olympelogin1.bullx>
2021-05-24 13:55:05 +0200

    slurm update

--- slurmscript.cmd
+++ slurmscript.cmd
@@ -6,11 +6,8 @@
 #SBATCH --ntasks-per-core=1
 #SBATCH -o main.out
 #SBATCH -e main.log
-#SBATCH --time=24:00:00
+#SBATCH --time=01:00:00
 #SBATCH --mail-user=erwanlecarpentier@mailbox.org
 #SBATCH --mail-type=END
 
-source venv/bin/activate
-python main.py
-deactivate
-
+julia scripts/dualcgp.jl
