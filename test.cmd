8b707f511552637ac493ca07a1902d81fdb67f27
erwanlecarpentier <erwan.lc2@gmail.com>
2021-05-26 18:02:57 +0200

    cmd updated

--- test.cmd
+++ test.cmd
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH -J main-exp
 #SBATCH -N 1
-#SBATCH -n 2
-#SBATCH --ntasks-per-node=2
+#SBATCH -n 9
+#SBATCH --ntasks-per-node=9
 #SBATCH --ntasks-per-core=1
 #SBATCH -o test.out
 #SBATCH -e test.log
-#SBATCH --time=00:01:00
+#SBATCH --time=00:10:00
 #SBATCH --mail-user=erwanlecarpentier@mailbox.org
 #SBATCH --mail-type=END
 
