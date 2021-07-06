6c45361ec58c7b19abadfabef3d09c192d634add
erwanlecarpentier <erwan.lc2@gmail.com>
2021-07-06 16:32:23 +0200

    Clean-up deprecated processing functions

--- src/process.jl
+++ src/process.jl
@@ -1,39 +1,6 @@
 export process, process_f
 
 """
-DEPRECATED
-
-    process(encoder::CGPInd, controller::CGPInd, inputs::AbstractArray, features_size::Int64)
-
-Process function chaining encoder and controller CGP individuals.
-Mean pooling layer is used in between both individuals.
-"""
-function process(encoder::CGPInd, controller::CGPInd, inp::AbstractArray,
-                 features_size::Int64)
-    process_f(encoder, controller, inp, features_size)[2]
-end
-
-"""
-DEPRECATED
-
-    process(encoder::CGPInd, controller::CGPInd, inputs::AbstractArray, features_size::Int64)
-
-Process function chaining encoder and controller CGP individuals.
-Mean pooling layer is used in between both individuals.
-Return both the processed output and the computed feature images.
-"""
-function process_f(encoder::CGPInd, controller::CGPInd, inp::AbstractArray,
-                 features_size::Int64)
-    out = CartesianGeneticProgramming.process(encoder, inp)
-    features = Array{Array{Float64}}(undef, length(encoder.outputs))
-    for i in eachindex(out)
-        features[i] = ReducingFunctions.mean_pool_reduction(out[i], features_size)
-    end
-    features_flatten = collect(Iterators.flatten(features))
-    return features, CartesianGeneticProgramming.process(controller, features_flatten)
-end
-
-"""
     function process(
         encoder::CGPInd,
         reducer::AbstractReducer,
