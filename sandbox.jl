5adc1aaa57e9bf5bc5c26d24d9cd641d41ae3ee1
erwanlecarpentier <erwanlecarpentier@mailbox.org>
2021-08-19 16:12:55 +0200

    fix

--- sandbox.jl
+++ sandbox.jl
@@ -2,14 +2,26 @@ using Random
 
 function job()
     Random.seed!(0)
-    out = rand(100000)
-    println("Thread: ", Threads.threadid(), " out: ", out[end-2:end])
+    n = 10000
+    out = Array{Float64,1}(undef, n)
+    for i in 1:n
+        out[i] = rand()
+    end
+    out
 end
 
 n_th = Threads.nthreads()
+n_out = 2 * n_th
+res = Array{Array{Float64,1},1}(undef, n_out)
 println()
-@sync for i in 1:n_th
+@sync for i in 1:n_out
     Threads.@spawn begin
-        job()
+        res[i] = job()
     end
 end
+
+all_same  = true
+for i in eachindex(res)
+    all_same *= res[i] == res[1]
+end
+println("All same: ", all_same)
