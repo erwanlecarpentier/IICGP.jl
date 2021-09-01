2429b8f1539ecb7066573e860a416e964dcca608
erwanlecarpentier <erwanlecarpentier@mailbox.org>
2021-09-01 16:12:55 +0200

    Integrated working multithreading to actual evaluation

--- src/evaluation.jl
+++ src/evaluation.jl
@@ -33,18 +33,19 @@ TODOs:
 - handle multi dimensional fitness in coevolution
 """
 function fitness_evaluate(e::DualCGPEvolution, fitness::Function=null_evaluate)
-    n_encoders = e.encoder_config.n_population
-    n_controllers = e.controller_config.n_population
-    fitness_matrix = Array{Float64}(undef, n_encoders, n_controllers)
+    n_enco = e.encoder_config.n_population
+    n_cont = e.controller_config.n_population
+    fitness_matrix = Array{Float64}(undef, n_enco, n_cont)
 
     println("###### Enter fitness evaluate") # TODO remove
     flush(stdout) # TODO remove
 
 	# FORMER method with @sync
-    @sync for i in 1:n_encoders
-        for j in 1:n_controllers
-	    println("###### About to do evaluation $i $j") # TODO remove
-	    flush(stdout) # TODO remove
+    #=
+	@sync for i in 1:n_enco
+        for j in 1:n_cont
+		    println("###### About to do evaluation $i $j") # TODO remove
+		    flush(stdout) # TODO remove
             encoder_i = IPCGPInd(e.encoder_config, e.encoder_population[i].chromosome)
             controller_j = CGPInd(e.controller_config, e.controller_population[j].chromosome)
             Threads.@spawn begin
@@ -52,8 +53,18 @@ function fitness_evaluate(e::DualCGPEvolution, fitness::Function=null_evaluate)
             end
         end
     end
+	=#
 
 	# NEW method with Threads.@threads
+	indexes = [(i, j) for i in 1:n_enco for j in 1:n_cont]
+    Threads.@threads for l in 1:(n_enco+n_cont)
+        i, j = indexes[l]
+		println("###### About to do evaluation $i $j") # TODO remove
+		flush(stdout) # TODO remove
+		enco_i = IPCGPInd(e.encoder_config, e.encoder_population[i].chromosome)
+		cont_j = CGPInd(e.controller_config, e.controller_population[j].chromosome)
+		fitness_matrix[i,j] = fitness(enco_i, cont_j)[1] # Currently, only pick 1st fitness dimension
+    end
 
     println("###### Completed evaluation") # TODO remove
     flush(stdout) # TODO remove
