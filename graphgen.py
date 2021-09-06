3354604934dfedc357447570bc083e75255687c5
erwanlecarpentier <erwan.lc2@gmail.com>
2021-09-06 19:21:30 +0200

    Python graph generator script

--- graphgen.py
+++ graphgen.py
@@ -1,26 +1,86 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import PIL
+import os
+
+DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+IMG_DIR = DIR_PATH + "/images/"
+fname = IMG_DIR + "lost_luggage_frame_31.png"
+
+img = plt.imread(fname)
+
 
 G = nx.Graph()
+G.add_node(0, image=img)
+G.add_node(1, image=img)
+G.add_node(2, image=img)
+G.add_edge(0, 1)
 G.add_edge(1, 2)
-G.add_edge(1, 3)
-G.add_edge(1, 5)
-G.add_edge(2, 3)
-G.add_edge(3, 4)
-G.add_edge(4, 5)
+G.add_edge(2, 0)
 
-# explicitly set positions
-pos = {1: (0, 0), 2: (-1, 0.3), 3: (2, 0.17), 4: (4, 0.255), 5: (5, 0.03)}
+# Get a reproducible layout and create figure
+pos = nx.spring_layout(G, seed=123)
+# pos = {1: (0, 0), 2: (-1, 0.3)}
+fig, ax = plt.subplots()
+ax.set_aspect('equal')
+plt.xlim(-1.5,1.5)
+plt.ylim(-1.5,1.5)
 
-options = {
-    "font_size": 36,
-    "node_size": 3000,
-    "node_color": "white",
-    "edgecolors": "black",
-    "linewidths": 5,
-    "width": 5,
+
+graph_options = {
+	"pos": pos,
+	"ax": ax,
+ 	"font_size": 36,
+	"node_size": 3000,
+	"node_color": "white",
+	"edgecolors": "black",
+	"linewidths": 5,
+	"width": 5,
 }
-nx.draw_networkx(G, pos, **options)
+edge_options = {
+	"pos": pos,
+	"ax": ax,
+	"arrows": True,
+	"arrowstyle": "-",
+#	"min_source_margin"=15,
+#	"min_target_margin"=15,
+}
+
+# Note: the min_source/target_margin kwargs only work with FancyArrowPatch objects.
+# Force the use of FancyArrowPatch for edge drawing by setting `arrows=True`,
+# but suppress arrowheads with `arrowstyle="-"`
+nx.draw_networkx_edges(G, **edge_options)
+# nx.draw_networkx(G, **graph_options)
+
+# Transform from data coordinates (scaled between xlim and ylim) to display coordinates
+tr_figure = ax.transData.transform
+# Transform from display to figure coordinates
+tr_axes = fig.transFigure.inverted().transform
+
+# Select the size of the image (relative to the X axis)
+img_size = (ax.get_xlim()[1] - ax.get_xlim()[0]) * 0.1
+img_center = img_size / 2.0
+
+# Add the respective image to each node
+for n in G.nodes:
+	xf, yf = tr_figure(pos[n])
+	print(pos[n])
+	print(xf, yf)
+	print()
+	xa, ya = tr_axes((xf, yf))
+	# get overlapped axes and plot icon
+	a = plt.axes([xa - img_center, ya - img_center, img_size, img_size])
+	a.imshow(G.nodes[n]["image"])
+	a.axis("off")
+ax.axis("off")
+plt.show()
+
+
+exit()
+
+
+
+
 
 # Set margins for the axes so that nodes aren't clipped
 ax = plt.gca()
