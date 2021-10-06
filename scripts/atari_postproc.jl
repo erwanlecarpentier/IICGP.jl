4da057be8991592ad56d499588ad911f93f76786
erwanlecarpentier <erwanlecarpentier@mailbox.org>
2021-10-06 09:55:57 +0200

    Postproc variables out of script

--- scripts/atari_postproc.jl
+++ scripts/atari_postproc.jl
@@ -1,25 +1,24 @@
 using IICGP
 using Dates
 
-rootdir = "/home/wahara/Documents/git/ICGP-results/"
-#rootdir = "/home/opaweynch/Documents/git/ICGP-results/"
+#rootdir = "/home/wahara/Documents/git/ICGP-results/"
+rootdir = "/home/opaweynch/Documents/git/ICGP-results/"
 resdir = joinpath(rootdir, "results/")
-# fetch_backup(rootdir, clean=true)
+#fetch_backup(rootdir, clean=true)
 
 min_date = DateTime(2021, 09, 01)
-max_date = DateTime(2021, 09, 30)
+max_date = DateTime(2021, 10, 02)
 # DateTime(2013,7,1,12,30,59,1)
 # games = Array{String,1}()
 games = ["boxing"] # ["freeway"]  # pong kung_fu_master freeway assault
-games_A = ["boxing" "assault" "freeway" "solaris" "defender" "gravitar" "space_invaders" "private_eye" "asteroids" "breakout" "frostbite" "riverraid"]
+games_12 = ["boxing" "assault" "freeway" "solaris" "defender" "gravitar" "space_invaders" "private_eye" "asteroids" "breakout" "frostbite" "riverraid"]
 reducers = Array{String,1}() # ["pooling"]
-
+dotime = false
+dosave = true
 unfinished = ["space_invaders" "private_eye" "frostbite" "riverraid"]
 
-for g in setdiff(games_A, unfinished)
+for g in unfinished
     exp_dirs, games = get_exp_dir(resdir, min_date=min_date, max_date=max_date,
                                   games=[g], reducers=reducers)
-    dotime = false
-    dosave = true
     process_results(exp_dirs, games, dotime, dosave, ma=1)
 end
