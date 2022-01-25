e35be80e4cfb7ddad6d615bd8a68cb50a27b0997
erwanlecarpentier <erwanlecarpentier@mailbox.org>
2022-01-25 18:29:48 +0100

    LUCIE games

--- atarisrun_lucie.cmd
+++ atarisrun_lucie.cmd
@@ -16,14 +16,14 @@ cd ~/IICGP.jl/
 USERNAME=$(whoami)
 OUTDIR="/tmpdir/$USERNAME/ICGP-results/results"
 CFG="cfg/eccgp_atari_lucie.yaml"
-GAMES="solaris boxing space_invaders"
+GAMES="solaris boxing space_invaders gravitar freeway asteroids"
 SCRIPT="scripts/atari_lucie.jl"
 PROJECT="$PWD"
 NINSTANCES=3
 
 for GAME in $GAMES; do
 	for i in $(seq 1 $NINSTANCES); do
-		echo srun -N 1 -n 2 julia --threads 2 --project=$PROJECT $SCRIPT --cfg=$CFG --game=$GAME --out=$OUTDIR
+		srun -N 1 -n 2 julia --threads 2 --project=$PROJECT $SCRIPT --cfg=$CFG --game=$GAME --out=$OUTDIR
 		sleep 0.1
 	done
 done
