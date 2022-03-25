527c1449dd80fb04f9aa72b3cb69391594251ea1
erwanlecarpentier <erwan.lc2@gmail.com>
2022-03-25 18:37:47 +0100

    All histograms

--- scripts/postproc_lucie_archi.jl
+++ scripts/postproc_lucie_archi.jl
@@ -3,8 +3,69 @@ using CartesianGeneticProgramming
 using IICGP
 using Dates
 using Plots
+#using LaTeXStrings
 
 IType = Union{Int16,Int32,Int64}
+
+function plot_histograms(
+    d::Dict{Any,Any};
+    bins::UnitRange{Int64}=0:15,
+    xticks::Vector{Int64}=[0,15],
+    yticks::Vector{Int64}=[0,8],
+    enco_color::Symbol=:firebrick1,
+    cont_color::Symbol=:paleturquoise3,
+    linecolor::Symbol=:white,
+    linewidth::Int64=2,
+    legend::Bool=false,
+    xtickfontsize::Int64=20,
+    ytickfontsize::Int64=20,
+    do_display::Bool=true,
+    do_save::Bool=true
+)
+    games = keys(d)
+    hs = []
+    for g in games
+        ids = keys(d[g])
+        n_enco_op = Vector{Int64}()
+        n_cont_op = Vector{Int64}()
+        for id in ids
+            n_op_per_action = d[g][id]["n_op_per_action"]
+            println(n_op_per_action)
+            for a in keys(n_op_per_action)
+                push!(n_enco_op, n_op_per_action[a]["enco"])
+                push!(n_cont_op, n_op_per_action[a]["cont"])
+            end
+        end
+        max_n_op = max(maximum(n_enco_op), maximum(n_cont_op))
+        @assert maximum(bins) > max_n_op
+        max_freq = 0
+        for n in 0:max_n_op
+            max_freq = max(max_freq, count(==(n), n_enco_op)+count(==(n), n_cont_op))
+        end
+        h = histogram()
+        for elt in [
+            [n_enco_op, "enco", enco_color],
+            [n_cont_op, "cont", cont_color]
+        ]
+            title = titlecase(replace(g,"_"=>" "))
+            yticks = :sparse # ([0,max_freq], [0,max_freq])
+            histogram!(h, elt[1], bins=bins, label=elt[2], color=elt[3],
+                linecolor=linecolor, linewidth=linewidth, legend=legend,
+                xtickfontsize=xtickfontsize, ytickfontsize=ytickfontsize,
+                xticks=xticks, yticks=yticks, title=title)
+        end
+        push!(hs, h)
+        if do_display
+            display(h)
+        end
+        if do_save
+            fname = string("n_op_", g, ".pdf")
+            savefig(h, fname)
+        end
+    end
+    #legend = plot([0 0], showaxis=false, grid=false, label=["Encoder operators" "Controller operators"])
+    plot(hs...) #, legend)
+end
 
 function recur_count_operation!(
     n_op_per_action::Dict{Any,Any},
@@ -41,9 +102,10 @@ function recur_count_operation!(
     end
 end
 
-
-function archi_analysis(
+function scrap_data!(
+    d::Dict{Any,Any},
     rom_name::String,
+    id::Int64,
     enco::CGPInd,
     redu::Reducer,
     cont::CGPInd,
@@ -66,6 +128,7 @@ function archi_analysis(
             recur_count_operation!(n_op_per_action, "enco", cfg, enco, a, outenc_node_index, seen_node_indexes)
         end
     end
+    d[rom_name][id] = Dict("n_op_per_action"=>n_op_per_action)
     if verbose
         println("\nNumber of operations per actions:")
         for k in keys(n_op_per_action)
@@ -79,27 +142,63 @@ function archi_analysis(
     end
 end
 
+function get_exp3_dirs(rootdir::String, resdir::String)
+    ids = [1,2,3]
+    reducers = ["pooling"]
+    min_date = DateTime(2022, 02, 23)
+    max_date = DateTime(2022, 02, 24)
+    games = ["alien", "bowling", "enduro", "pong", "riverraid", "seaquest"]
+    exp_dirs, ids, rom_names = get_exp_dir(resdir, min_date=min_date, max_date=max_date,
+        games=games, reducers=reducers, ids=ids)
+    min_date = DateTime(2022, 02, 08, 15)
+    max_date = DateTime(2022, 02, 08, 17)
+    games = ["boxing", "asteroids", "solaris", "freeway", "gravitar", "space_invaders"]
+    e, i, r = get_exp_dir(resdir, min_date=min_date, max_date=max_date,
+        games=games, reducers=reducers, ids=ids)
+    push!(exp_dirs, e...)
+    push!(ids, i...)
+    push!(rom_names, r...)
+    exp_dirs, ids, rom_names
+end
+
 rootdir = joinpath(homedir(), "Documents/git/ICGP-results/")
 resdir = joinpath(rootdir, "results/")
+exp_dirs, ids, rom_names = get_exp3_dirs(rootdir, resdir)
+
+
 min_date = DateTime(2022, 02, 23) # DateTime(2022, 02, 08, 15)
 max_date = DateTime(2022, 02, 24) # DateTime(2022, 02, 08, 16)
-#games = ["boxing", "asteroids", "breakout", "freeway", "gravitar", "riverraid", "space_invaders"]
-games = ["bowling"]
-ids = [1]
+games = ["pong", "enduro"]
+ids = [1,2,3]
 reducers = ["pooling"]
-exp_dirs, ids, games = get_exp_dir(resdir, min_date=min_date, max_date=max_date,
+exp_dirs, ids, rom_names = get_exp_dir(resdir, min_date=min_date, max_date=max_date,
     games=games, reducers=reducers, ids=ids)
-max_frames = 18000 # 18000
-render_graph = false
-seed = 0
 
-# 1. Process each ind
+
+d = Dict() # Contains all the data
+for g in rom_names
+    d[g] = Dict()
+end
+
+# 1. Scrap data for each individual
 for i in eachindex(exp_dirs)
     # Fetch best individual
     exp_dir = exp_dirs[i]
-    rom_name = games[i]
+    id = ids[i]
+    rom_name = rom_names[i]
     cfg = cfg_from_exp_dir(exp_dir)
     enco, redu, cont = get_best_lucie_ind(exp_dir)
     # Get analysis
-    archi_analysis(rom_name, enco, redu, cont, cfg)
+    scrap_data!(d, rom_name, id, enco, redu, cont, cfg, verbose=false)
 end
+
+# 2. Plot data
+plot_histograms(d)
+
+
+
+##
+
+legend = plot([0 0], showaxis=false, grid=false, label=["Encoder operators" "Controller operators"])
+plt = plot(legend)
+display(legend)
